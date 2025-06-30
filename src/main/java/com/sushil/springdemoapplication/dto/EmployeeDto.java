804c270a702c1a53cc038961707931e25ce4b15c
sushilv3 <sushilv3rma@gmail.com>
2025-06-30 19:21:07 +0530

    added entity and dto

---
+++ src/main/java/com/sushil/springdemoapplication/dto/EmployeeDto.java
@@ -0,0 +1,31 @@
+package com.sushil.springdemoapplication.dto;
+
+public class EmployeeDto {
+    String name;
+    String dept;
+    Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
---
+++ src/main/java/com/sushil/springdemoapplication/entity/Employee.java
@@ -0,0 +1,45 @@
+package com.sushil.springdemoapplication.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+    @Id
+    Integer id;
+    String name;
+    String dept;
+    Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
